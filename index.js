cbd5a8777b3990ad59681534b4b4997aabe96930
harshita <harshita1225@gmail.com>
2022-07-18 19:12:31 +0200

    task pending

--- index.js
+++ index.js
@@ -1,21 +1,51 @@
 // 1
 // Write a JavaScript function that accepts a string as a parameter and counts the number of vowels within the string.
 function vowel_count(str1) {
-  //write your code here
+  let vowelCount = 0;
+  let letters = str1.split("");
+  console.log(letters);
+
+  for (let i = 0; i < letters.length; i++) {
+    if (letters[i] === "a") {
+      vowelCount++;
+    }
+    if (letters[i] === "e") {
+      vowelCount++;
+    }
+    if (letters[i] === "i") {
+      vowelCount++;
+    }
+    if (letters[i] === "o") {
+      vowelCount++;
+    }
+    if (letters[i] === "u") {
+      vowelCount++;
+    }
+  }
+  return vowelCount;
 }
 console.log(vowel_count("The quick brown fox"));
-
+console.log(
+  vowel_count("she sells sea shells on the sea shore when the sun shines")
+);
 //2
 // Write a JavaScript function that generates a string id (specified length) of random characters.
 function makeid(l) {
-  //write your code here
+  let randomstr = "";
+  let mainstr =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < l; i++) {
+    randomstr += mainstr.charAt(Math.floor(Math.random() * mainstr.length));
+  }
+
+  return randomstr;
 }
 console.log(makeid(8));
+console.log(makeid(36));
+console.log(makeid(12));
 
 // 3
 // Write a JavaScript program to pass a 'JavaScript function' as parameter.
-function abc() {
-  //write your code here
-}
+function abc() {}
 
 abc();
